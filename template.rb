d10d6ef84fb44aae2d8a16b171fa186ae28c5251
Simão Belchior <simao.belchior@mindera.com>
2026-04-09 14:38:16 +0100

    Remove rails default gems

--- template.rb
+++ template.rb
@@ -4,37 +4,10 @@
 # Usage:
 #   rails new myapp -d postgresql --template=template.rb
 
-# Gemfile setup
-gem "propshaft"
+# Gemfile setup - only adding gems not included by default in Rails 8.1
 gem "dartsass-rails"
 gem "bootstrap", "~> 5.3"
-gem "importmap-rails"
-gem "turbo-rails"
-gem "stimulus-rails"
 gem "devise", "~> 4.9"
-gem "solid_cache"
-gem "solid_queue"
-gem "solid_cable"
-gem "bootsnap", require: false
-gem "kamal", require: false
-gem "thruster", require: false
-gem "image_processing", "~> 1.2"
-
-gem_group :development, :test do
-  gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
-  gem "bundler-audit", require: false
-  gem "brakeman", require: false
-  gem "rubocop-rails-omakase", require: false
-end
-
-gem_group :development do
-  gem "web-console"
-end
-
-gem_group :test do
-  gem "capybara"
-  gem "selenium-webdriver"
-end
 
 after_bundle do
   # Install Devise
